c73fb5a6dd93a7b7cb388c292df328cbd85a5b45
Siyara Wijewardane <siyarawijewardane@gmail.com>
2024-07-27 23:44:55 +0530

    putting together(1)

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,14 +1,20 @@
+import SidePanel from "@components/dashboard/side_panel/SidePanel";
 import Tab from "@components/tab_bar/Tab";
 
 export default function Dashboard() {
   return (
-    <div className="flex">
-      
-      <Tab to="/dashboard" content = "Dashboard" />
-      <Tab to="/" content = "Project 1" />
-      <Tab to="/dashboard" content = "Project 2" />
-      
-      
+    <div>
+      <div className="flex">
+        
+        <Tab to="/dashboard" content = "Dashboard" />
+        <Tab to="/" content = "Project 1" />
+        <Tab to="/dashboard" content = "Project 2" />
+       
+      </div>
+      <div>
+        <SidePanel/>
+      </div>
     </div>
+    
   );
 }
