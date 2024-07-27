99878f5cde3ac1cc68ffdb4ab56616db9b67ddf5
Siyara Wijewardane <siyarawijewardane@gmail.com>
2024-07-27 13:05:35 +0530

    tabs created

--- components/tab_bar/Tab.jsx
+++ components/tab_bar/Tab.jsx
@@ -1,7 +1,32 @@
-export default function TabBar() {
-    return (
-      <div>
-        This is tab bar
+"use client";
+
+import Link from 'next/link';
+import { useState } from 'react';
+
+export default function Tab({ to, content }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div className='flex'>
+      <Link
+        href={to}
+        className="relative flex items-center justify-start w-32 px-4 py-2 overflow-hidden text-white text-11px bg-primary"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <div className="w-full overflow-hidden whitespace-nowrap">
+          {content}
+        </div>
+    
+        {isHovered && (
+          <span className="absolute text-white right-4">&times;</span>
+        )}
+
+      
+      </Link>
+      <div className='relative flex items-center bg-black'>
+        <div className='w-px bg-white h-1/2' />
       </div>
-    );
+    </div>
+  );
 }
