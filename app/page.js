1c2e1e674cdc0a0f66a76aaba1920d4c94bc07d3
Siyara Wijewardane <siyarawijewardane@gmail.com>
2024-07-28 10:35:27 +0530

    merging

--- app/page.js
+++ app/page.js
@@ -1,15 +1,11 @@
 import Image from "next/image";
 import Header from "@components/header";
-<<<<<<< HEAD
 import SidePanel from "@components/dashboard/side_panel/SidePanel";
-=======
 import Utils from "@components/dashboard/utility_bar/Utils";
->>>>>>> ed6efc40d89f9a5cb9452d0827d1e3ef48a08956
 
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
 
       <SidePanel />
       
@@ -17,9 +13,7 @@ export default function Home() {
       
      
 
-=======
       <Utils />
->>>>>>> ed6efc40d89f9a5cb9452d0827d1e3ef48a08956
     </div>
   );
 }
