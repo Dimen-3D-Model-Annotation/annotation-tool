a456780f226ecbe4d541feed3e4c680a1c8905f5
vishwanthieherath <vishwanthieherath@gmail.com>
2024-11-23 05:42:14 +0530

    Update Canvas Component

--- components/project_workspace/canvas/CanvasArea.jsx
+++ components/project_workspace/canvas/CanvasArea.jsx
@@ -1,14 +1,14 @@
 "use client";
 
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { FileContext } from "@contexts/FileContext";
-import { CommentContext } from "@contexts/CommentContext";
+import { FileProvider } from "@contexts/FileContext";
+import { CommentContext, CommentProvider } from "@contexts/CommentContext";
 
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Html } from "@react-three/drei";
-
-import Object from "@components/project_workspace/canvas/Object.jsx";
+import Model from "./Model";
 
 const Comment = ({ position, text }) => (
   <mesh position={[position.x, position.y, position.z]}>
@@ -36,31 +36,24 @@ const CanvasArea = () => {
   // };
 
   return (
-    <div className="bg-zinc-800 flex justify-center items-center min-h-screen px-[250px] pt-[70px]">
-      <div className="flex justify-center items-center h-[500px] w-[100%]">
-        <Canvas>
-          <Suspense fallback={null}>
-            {/* <Html>
-              {filePreview && (
-                <img
-                  src={filePreview}
-                  alt="Uploaded File"
-                  style={{ maxWidth: "100%", height: "auto" }}
+    <FileProvider>
+      <CommentProvider>
+        <div className="bg-zinc-800 flex justify-center items-center min-h-screen px-[200px] pb-[10px] pt-[60px]">
+          <div className="flex justify-center items-center h-[625px] w-[100%] z-10">
+            <Suspense fallback={null}>
+              {filePreview ? <Model fileUrl={filePreview} /> : null}
+              {comments.map((comment, index) => (
+                <Comment
+                  key={index}
+                  position={comment.position}
+                  text={comment.text}
                 />
-              )}
-          </Html> */}
-            <Object />
-            {comments.map((comment, index) => (
-              <Comment
-                key={index}
-                position={comment.position}
-                text={comment.text}
-              />
-            ))}
-          </Suspense>
-        </Canvas>
-      </div>
-    </div>
+              ))}
+            </Suspense>
+          </div>
+        </div>
+      </CommentProvider>
+    </FileProvider>
   );
 };
 
