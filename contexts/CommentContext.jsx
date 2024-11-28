fa8ac371b180bf9591923a22028407bc451ea819
vishwanthieherath <vishwanthieherath@gmail.com>
2024-11-28 12:45:39 +0530

    Update CommentContext

--- contexts/CommentContext.jsx
+++ contexts/CommentContext.jsx
@@ -6,13 +6,15 @@ const CommentProvider = ({ children }) => {
   const [commentMode, setCommentMode] = useState(false);
   const [comments, setComments] = useState([]);
 
+  const [clickedPoint, setClickedPoint] = useState(null);
+
   const addComment = (comment) => {
     setComments([...comments, comment]);
   };
 
   return (
     <CommentContext.Provider
-      value={{ commentMode, setCommentMode, comments, addComment }}
+      value={{ commentMode, setCommentMode, comments, addComment, clickedPoint, setClickedPoint }}
     >
       {children}
     </CommentContext.Provider>
